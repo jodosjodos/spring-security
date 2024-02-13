6bedc674db0a8afb5f41a4558707a504583715b9
jodosjodos <jeandedieu2030@gmail.com>
2024-02-13 05:59:26 +0200

    change port

--- src/main/java/com/springSecurity/controller/AuthenticationController.java
+++ src/main/java/com/springSecurity/controller/AuthenticationController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-
+//sign up controller
     @Operation(
             description = " Register user and send email  that notify you that you have created account and you  with credentials you  have created with ",
             summary = "this is summary for  register user with email, password , phone number and full name",
